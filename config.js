cda96d62b1a7063d5c1479b4e8eeda1a88b0d3ed
nn <205666415+guessmesome@users.noreply.github.com>
2025-11-12 12:11:02 +0700

    upd

---
+++ config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+    products: {
+        1: 'https://vldmnt.ru/go/srdjtuk9qt',
+        2: 'https://vldmnt.ru/go/salxin4k1k',
+        3: 'https://vldmnt.ru/go/s4hpzz2f7v',
+        4: 'https://vldmnt.ru/go/sg5m46trfz'
+    }
+};
+
